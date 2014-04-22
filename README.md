3fc075f2978756c28084eedfeb90d5c9455807e9
Jordan Thomas <jthomas@barefootproximity.com>
2014-04-22 13:57:17 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,37 @@
+Massive Hipster
+===============
+
+A website generator on Gulp. I use it to create HTML wireframes, prototypes, that sort of thing.
+
+## Why "Massive Hipster"?
+
+That's what Github suggested. I couldn't think of anything better, so, Massive Hipster it was.
+
+## Get started
+
+0. Install gulp if you haven’t already, `npm install -g gulp`
+1. `git clone git@github.com:jordanthomas/massive-hipster.git [project_name]`
+2. `cd [project_name]`
+3. `npm install`
+4. `gulp`
+5. Enjoy.
+
+**Note, at this point you _probably_ want to remove the `origin` remote and add your own so you can save your work to your own repo.**
+
+## Tasks
+
+`gulp default` - Starts a server and watcher.
+
+`gulp clean` - Removes the `build` directory. Run before building for production to clean out any old files.
+
+`gulp build` - Like the default task, but without a server or watcher.
+
+## Uses
+
+- Sass (via libass, install it with Homebrew)
+- Bourbon
+- Neat
+- Jade
+- Normalize
+- Font Awesome
+
---
+++ gulpfile.js
@@ -0,0 +1,83 @@
+var ecstatic    = require('ecstatic'),
+    http        = require('http'),
+    path        = require('path'),
+    gulp        = require('gulp'),
+    concat      = require('gulp-concat'),
+    jshint      = require('gulp-jshint'),
+    uglify      = require('gulp-uglify'),
+    jade        = require('gulp-jade'),
+    sass        = require('gulp-sass'),
+    imagemin    = require('gulp-imagemin'),
+    clean       = require('gulp-clean'),
+    paths = {
+      build:     'build',
+      scripts:   ['source/js/**/*.js', '!source/js/vendor/**/*.js'],
+      pages:     ['source/**/*.jade', '!source/layouts/**'],
+      layouts:   ['source/layouts/**/*.jade'],
+      scss_main: ['source/css/main.scss'],
+      scss:      ['source/css/**/*.scss'],
+      fonts:     ['source/css/fonts/**'],
+      images:    ['source/img/**/*.jpg', 'source/img/**/*.jpeg', 'source/img/**/*.png']
+    };
+
+gulp.task('uglify', function() {
+  return gulp.src(paths.scripts)
+    .pipe(uglify())
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest(path.join(paths.build, 'js')));
+});
+
+gulp.task('jslint', function() {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+});
+
+gulp.task('scss', function () {
+  return gulp.src(paths.scss_main)
+    .pipe(sass({ errLogToConsole: true }))
+    .pipe(gulp.dest(path.join(paths.build, 'css')));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(paths.fonts)
+    .pipe(gulp.dest(path.join(paths.build, 'css', 'fonts')));
+});
+
+gulp.task('images', function () {
+  return gulp.src(paths.images)
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.join(paths.build, '/img')));
+});
+
+gulp.task('html', function() {
+  return gulp.src(paths.pages)
+    .pipe(jade({ pretty: true }))
+    .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('server', function() {
+  http.createServer(
+    ecstatic({ root: path.join(__dirname, paths.build) })
+  ).listen(8080);
+
+  console.log('Server listening on 8080...');
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['jslint', 'uglify']);
+  gulp.watch(paths.scss, ['scss']);
+  gulp.watch(paths.fonts, ['fonts']);
+  gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.pages, ['html']);
+  gulp.watch(paths.layouts, ['html']);
+});
+
+gulp.task('clean', function() {
+  return gulp.src(paths.build, { read: false })
+    .pipe(clean());
+});
+
+gulp.task('build', ['html', 'scss', 'fonts', 'uglify', 'images']);
+
+gulp.task('default', ['server', 'html', 'scss', 'fonts', 'jslint', 'uglify', 'images', 'watch']);
